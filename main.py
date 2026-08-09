6e50745534af10f4f9099456f97d11bf31cc9c4c
Onekuys <mykhailo.yanyev@gmail.com>
2026-08-09 12:27:50 +0200

    added an asteroid spawner

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -22,8 +34,10 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen) 
+        updatable.update(dt)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
